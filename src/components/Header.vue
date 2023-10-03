e91e32940bc1b78c8b380c08c90cbd8e0202fd7d
Romain Wirth <wirth.romain@gmail.com>
2023-10-03 11:52:38 +0200

    UPD environment + scss

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<script></script>
+
+<template>
+  <header class="display-flex-row justify-content-center">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
